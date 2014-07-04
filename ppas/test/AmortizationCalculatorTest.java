29d5f484cf5becf6f80b256b6b560a42ea288f95
garudareiga <garudareiga@gmail.com>
2014-07-03 23:44:20 -0700

    Add amortinzation code

---
+++ ppas/test/AmortizationCalculatorTest.java
@@ -0,0 +1,14 @@
+package amortization;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AmortizationCalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
